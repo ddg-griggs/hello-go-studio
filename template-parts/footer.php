4e18c58a68bd6b78f4c6376b6f235924c39cc6b0
DDG <dankovdeveloper@gmail.com>
2025-05-26 20:14:57 +0300

    footer.php

---
+++ template-parts/footer.php
@@ -0,0 +1,8 @@
+<footer class="go-footer">
+  <div class="go-container">
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
